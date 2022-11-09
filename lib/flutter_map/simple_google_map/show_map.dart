7234dc1ef3c508689dda5954a7d8e2b6f62a6d73
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-11-09 20:22:41 +0600

    Camera Position Animation

--- lib/flutter_map/simple_google_map/show_map.dart
+++ lib/flutter_map/simple_google_map/show_map.dart
@@ -9,7 +9,8 @@ class showMap extends StatefulWidget {
 
 class _showMapState extends State<showMap> {
   late GoogleMapController mapController;
-  static LatLng _center = const LatLng(23.7104, 90.4074);
+  static LatLng _center = const LatLng(23.816591317488747, 90.41560944927275);
+  static LatLng _anotherLocation = const LatLng(23.7088442434317, 90.40608696755409);
 
   void _onMapCreated(GoogleMapController controller){
     mapController = controller;
@@ -21,15 +22,32 @@ class _showMapState extends State<showMap> {
         title: Text("Flutter Map"),
       ),
       body: GoogleMap(
-        markers: {dhakaMarker},
+        markers: {dhakaMarker,dhakaMarkerSecond},
         mapType: MapType.terrain,
         onMapCreated: _onMapCreated,
         initialCameraPosition: CameraPosition(target: _center,zoom: 14.0),),
+        floatingActionButton: FloatingActionButton.extended(onPressed: _goToDU, label: Text("Dhaka University"),
+          icon: Icon(Icons.place)),
     );
   }
+  static final CameraPosition duPosition = CameraPosition(target: LatLng(23.7341698,90.3905615),
+      zoom: 14.0,
+  bearing: 150.0,
+  tilt: 35.0);
+  
+   Future<void>_goToDU()async{
+    GoogleMapController controller = await mapController;
+    controller.animateCamera(CameraUpdate.newCameraPosition(duPosition));
+   }
+
   Marker dhakaMarker = Marker(markerId: MarkerId("Dhaka"),
     position: _center,
     infoWindow: InfoWindow(title: "Dhaka",snippet: "This is the capital of BD"),
+    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+  );
+  Marker dhakaMarkerSecond = Marker(markerId: MarkerId("AhsanManZil"),
+    position: _anotherLocation,
+    infoWindow: InfoWindow(title: "Ahsan Manzil Museum",snippet: "Historical Place"),
     icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
   );
 }
